5ef440c10aa703f7276eab364c7759be8e38f700
desperial <premodialburst@gmail.com>
2017-05-16 15:01:51 +0500

    Инсталятор
    Создание таблицы в БД

---
+++ xml-parser/install.php
@@ -0,0 +1,41 @@
+<?php
+//Подключаемся к БД
+$mysqli = mysqli_init();
+  if (!$mysqli) {
+    die('mysqli_init завершилась провалом');
+  }
+
+  if (!$mysqli->options(MYSQLI_INIT_COMMAND, 'SET AUTOCOMMIT = 0')) {
+    die('Установка MYSQLI_INIT_COMMAND завершилась провалом');
+  }
+
+  if (!$mysqli->options(MYSQLI_OPT_CONNECT_TIMEOUT, 5)) {
+    die('Установка MYSQLI_OPT_CONNECT_TIMEOUT завершилась провалом');
+  }
+
+  if (!$mysqli->real_connect('host', 'username', 'pass', 'dbname', 'port')) {
+    die('Ошибка подключения (' . mysqli_connect_errno() . ') '
+            . mysqli_connect_error());
+  };
+  //Создание таблицы
+  //id - ключ
+  //adress - адрес объекта
+  //obj_type - тип объекта
+  //announce - описание объекта
+  //img - изображение объекта
+  //price - цена на объект
+  $query = "CREATE TABLE item (
+  	id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  	adress CHAR(50) NOT NULL,
+  	obj_type CHAR(10) NULL,
+  	announce TEXT(400) NULL,
+  	img CHAR(50) NULL,
+    price int(10) NULL
+  )";
+
+if (!$mysqli->query ($query)){
+  die('Не получилось создать таблицу');
+};
+
+$mysqli->close();
+?>
