cf6a5d333e65e8ff0d576a12d8523d98427c3c07
SteamFox <madfoxduran@gmail.com>
2017-05-17 16:22:14 +0500

    base functions for test

--- xml-parser/model.php
+++ xml-parser/model.php
@@ -1,8 +1,28 @@
 <?php
   class XMLdoc(){
-    public function Load(){}
-    public function Validate(){}
-    public function Compare(){}
+  	private $xmlFile;
+  	private $dbo;
+  	private $user = "root";
+  	private $pass = "L3zctxfg!";
+  	private function DBConnect() {
+  		try {
+  			$dbo = new PDO('mysql:host=localhost;dbname=realty',$user,$pass);
+  		}
+  		catch (PDOException $e) {
+		    echo "Error!: " . $e->getMessage() . "<br/>";
+		    die();
+		}
+
+  	}
+    public function Load($path){
+    	$xmlFile = simplexml_load_file($path);
+    }
+    public function Validate(){
+
+    }
+    public function Compare(){
+    	
+    }
     public function Cache(){}
     public function WriteInDB(){}
 
