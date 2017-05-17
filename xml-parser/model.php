93667d5345b5522972850de6a59f99902bc2906e
desperial <premodialburst@gmail.com>
2017-05-17 15:34:59 +0500

    +Модель

---
+++ xml-parser/model.php
@@ -0,0 +1,10 @@
+<?php
+  class XMLdoc(){
+    public function Load(){}
+    public function Validate(){}
+    public function Compare(){}
+    public function Cache(){}
+    public function WriteInDB(){}
+
+  }
+?>
